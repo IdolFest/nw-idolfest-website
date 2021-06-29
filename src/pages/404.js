7d0f2dd51d693e9c090a144f50d353a1e943b7cc
Shir Goldberg <me@shirgoldbird.com>
2021-06-28 19:27:03 -0700

    improve 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,19 @@
 import * as React from "react"
-
 import Layout from "@components/layout"
-import Seo from "../components/seo"
+import Seo from "@components/seo"
+import PageContent from '@components/PageContent'
+import PageHeader from '@components/PageHeader'
 
 const PageNotFound = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just found a page that doesn&#39;t exist... the sadness.</p>
+    <PageHeader 
+      title="404: Not Found" 
+    />
+
+    <PageContent>
+      <p>You just found a page that doesn&#39;t exist... the sadness.</p>
+    </PageContent>
   </Layout>
 )
 
