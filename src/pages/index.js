f59dc1890731684df0a135eb46298e62aac16f79
Shir Goldberg <me@shirgoldbird.com>
2021-07-06 15:21:50 -0700

    remove extraneous code

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,12 +20,6 @@ const IndexPage = () => (
     <PageContent>
       <p>The Northwest Idol Festival is a celebration of all things idol! Join us in Seattle on November 13-14, 2021 for two days of guests, concerts, panels, cosplay, and more. We are run by idol fans, for idol fans.</p>
       <p>Ready to find out more? You'll be able to buy your hotel room and badge soon!</p>
-      {/* <section>
-        <h3>Updates</h3>
-        <p>A list of blog posts will go here.</p>
-      </section>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> */}
     </PageContent>
   </Layout>
 )
