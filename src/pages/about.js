ebc0325cb0b7258a6df697b0da514d4e7ebcec0d
Shir Goldberg <me@shirgoldbird.com>
2021-07-23 11:02:20 -0400

    tweak text

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,11 +27,12 @@ const AboutPage = () => (
       
       <h2>The Co-Chair</h2>
       <p>
-        <strong>Shir Goldberg</strong> (<i>they/them</i>) is a long-time con organizer who's passionate about building events with a heart. Their goal at Northwest IdolFest is to support you in creating amazing things. Shir combines a professional background in consulting with years of convention experience, and has run multiple events that drew tens of thousands of attendees. In their spare time, Shir enjoys petting dogs. 
+        <strong>Shir Goldberg</strong> (<i>they/them</i>) is a long-time con organizer who's passionate about building events with a heart. Their goal at Northwest IdolFest is to support you in creating amazing things. Shir combines a professional background in tech consulting with years of convention experience, and has run multiple events that drew tens of thousands of attendees. In their spare time, Shir enjoys petting dogs. 
       </p>
 
       <h2>The Staff</h2>
-      <p>Our staff is made up of veteran convention runners from all backgrounds and professions. We operate under a policy of transparency and long-term planning to bring the best possible experience to the Pacific Northwest.</p>
+      <p>Our all-volunteer staff is made up of veteran convention runners from all backgrounds and professions. We operate under a policy of transparency and long-term planning to bring the best possible experience to the Pacific Northwest.</p>
+      <p>Have questions or want to join up? <a href='mailto:contact@nwidolfest.com' target='_blank' rel='noreferrer'>Email us!</a></p>
     </PageContent>
   </Layout>
 )
