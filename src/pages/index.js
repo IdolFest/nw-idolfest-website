bb36da7ab9aeed118239e6c17d61ae90b573bea7
Shir Goldberg <me@shirgoldbird.com>
2021-07-31 18:53:51 -0700

    update homepage text

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const IndexPage = () => {
 
       <PageContent>
         <p>Northwest Idol Festival is a two day event featuring idols, anisong, and everything in between. Join us in Seattle on November 13-14, 2021 for an exciting weekend of guests, concerts, panels, cosplay, and more. Get ready for a whole new idol experience!</p>
-        <p>Ready to find out more? <Link to="/hotel">Book your hotel room today</Link>, and sign up for our email list below to find out when badges go on sale.</p>
+        <p>Ready to find out more? <Link to="/hotel">Book your hotel room</Link> and <Link to="register">buy your badge</Link> today! You can also sign up for our email list below to find out more.</p>
         <NewsletterSignup />
       </PageContent>
     </Layout>
