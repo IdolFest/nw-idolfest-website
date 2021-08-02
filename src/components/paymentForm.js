d4325817dcc83a4e545e8a42578faf88f08bbaa0
Shir Goldberg <me@shirgoldbird.com>
2021-08-02 11:38:23 -0700

    show Discord link after reg

--- src/components/paymentForm.js
+++ src/components/paymentForm.js
@@ -115,7 +115,7 @@ function displayPaymentResults(status, paymentResults) {
         statusContainer.classList.add('is-success');
         receiptContainer.style.visibility = 'visible';
         receiptContainer.style.visibility = 'visible';
-        receiptContainer.innerHTML = `Thank you for purchasing a NWIF badge! Please save <a href=${paymentResults.payment.receiptUrl} target="_blank">your receipt</a> before closing this page.<br /><br />`;
+        receiptContainer.innerHTML = `Thank you for purchasing a NWIF badge! Please save <a href=${paymentResults.payment.receiptUrl} target="_blank">your receipt</a> before closing this page.<br /><br /><a href="https://discord.gg/h5yJbXgTgE" target="_blank">Join our Discord</a> and claim your role now!<br /><br />`;
     } else {
         statusContainer.classList.remove('is-success');
         statusContainer.classList.add('is-failure');
