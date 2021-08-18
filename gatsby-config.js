947313b49a3dbfc2b69b3fcc6f2684bc1439528f
Shir Goldberg <me@shirgoldbird.com>
2021-08-18 16:32:19 -0700

    add in plugin to convert links to Gatsby Links

--- gatsby-config.js
+++ gatsby-config.js
@@ -124,6 +124,7 @@ module.exports = {
         extensions: [`.md`, `.mdx`],
       },
     },
+    `gatsby-plugin-catch-links`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
