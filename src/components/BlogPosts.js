b34906c4e3213d278830d87caff294103d5d0a41
Shir Goldberg <me@shirgoldbird.com>
2021-08-18 17:15:11 -0700

    sort blog posts newest first

--- src/components/BlogPosts.js
+++ src/components/BlogPosts.js
@@ -5,7 +5,7 @@ export default function BlogPosts() {
     const { blog } = useStaticQuery(
         graphql`
             query MyQuery {
-                blog: allMdx(filter: {frontmatter: {template: {eq: "blogpost"}}}) {
+                blog: allMdx(sort: { fields: [frontmatter___date], order: DESC }, filter: {frontmatter: {template: {eq: "blogpost"}}}) {
                     posts: nodes {
                     fields {
                         slug
