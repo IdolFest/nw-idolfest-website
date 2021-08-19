2c514c181fba6e7f6a832dbc002ecf0bff129c31
Shir Goldberg <me@shirgoldbird.com>
2021-08-18 17:14:17 -0700

    update filesystem path for new blog location

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/src/blog`,
+        path: `${__dirname}/src/markdown-pages/blog`,
       },
     },
     {
