d831033916119f09ae7ce39bbf6d621914f1581e
Shir Goldberg <me@shirgoldbird.com>
2021-10-19 15:53:45 -0700

    add Ivory table pic

--- src/pages/artistalley/ivoryice.js
+++ src/pages/artistalley/ivoryice.js
@@ -4,6 +4,7 @@ import Seo from '@components/seo'
 import PageContent from '@components/PageContent'
 import PageHeader from '@components/PageHeader'
 import Guest from '@components/Guest'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IvoryIcePage = () => {
     const guestSocials = [
@@ -24,6 +25,14 @@ const IvoryIcePage = () => {
                 <Guest personName="Ivory Ice" guestSocials={guestSocials}>
                     Coffeeholic artist based in SoCal who has been traveling and selling at conventions since 2015! I sell a variety of items such as prints, wooden pins, buttons, stickers, and enamel pins!
                 </Guest>
+                <StaticImage
+                    // This is a presentational image, so the alt should be an empty string
+                    alt=''
+                    src='../../images/artists/ivory_table.png'
+                    loading='eager'
+                    placeholder='blurred'
+                    quality='100'
+                />
             </PageContent>
         </Layout>
     )
