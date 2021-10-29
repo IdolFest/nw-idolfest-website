98226199b9bf8ae70795a9bab269f6b1162be42c
Shir Goldberg <me@shirgoldbird.com>
2021-10-29 13:40:53 -0700

    update Megan bio

--- src/pages/guests/meganshipman.js
+++ src/pages/guests/meganshipman.js
@@ -25,7 +25,7 @@ const MeganShipmanPage = () => {
                     
                     <br /><br />She's voiced characters like La Brava from “My Hero Academia”, Gigi Andalucia from “Mobile Suit Gundam Hathaway”, Mayuri Shiina from "Steins;Gate 0", Grey from “Black Clover”, Homura from “Dr. Stone”, Maple from “BOFURI”, Yuzu Aihara from "Citrus", Aoba Suzukaze from "New Game!", Camie from "One Piece”, Mare Bello Fiore from "Overlord", Eripiyo from “If My Favorite Pop Idol Made It To The Budokan I Would Die”, and more!
                     
-                    <br /><br />As an ADR Director, Megan has directed shows like, “Show by Rock!! Mashumairesh!!”, “Scarlet Nexus”, “No Guns Life”, and much more!
+                    <br /><br />As an ADR Director, Megan has directed shows like “Love Live! Nijigasaki High School Idol Club”, “Show by Rock!! Mashumairesh!!”, “Scarlet Nexus”, “No Guns Life”, and much more!
                 </Guest>
             </PageContent>
         </Layout>
