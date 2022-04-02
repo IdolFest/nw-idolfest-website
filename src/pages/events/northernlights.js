34ed76e66762e832506a758275feaf7cab1f15ac
Shir Goldberg <me@shirgoldbird.com>
2022-04-02 16:17:36 -0700

    make link more obvious

--- src/pages/events/northernlights.js
+++ src/pages/events/northernlights.js
@@ -14,8 +14,8 @@ const NorthernLightsPage = () => {
         <PageContent>
         <p>
             What's an idol event without music? The Northern Lights Music Festival will be returning in 2022, bigger and better than ever!
-            <br />
-            If you'd like to apply, fill out <a href="https://idolfe.st/NL2022" target="_blank" rel="noreferrer">the application</a> by August 1st, 2022.
+            <br /><br />
+            If you'd like to apply, fill out <a href="https://idolfe.st/NL2022" target="_blank" rel="noreferrer">the application</a> by August 1st, 2022. You'll find it at <a href="https://idolfe.st/NL2022" target="_blank" rel="noreferrer">https://idolfe.st/NL2022</a>.
         </p>
         </PageContent>
     </Layout>
