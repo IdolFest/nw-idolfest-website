b0a6bad59c5ce70863f30bd7d8ad9f00ba984815
Shir Goldberg <me@shirgoldbird.com>
2022-05-18 00:15:38 -0600

    add hero photo collage to site

--- src/components/hero.js
+++ src/components/hero.js
@@ -23,18 +23,19 @@ export default function Hero({ header, body }) {
       <StaticImage
         style={{
           gridArea: '1/1',
-          maxHeight: '50vh'
+          maxHeight: '80vh',
         }}
-        layout='fullWidth'
-        aspectRatio={16 / 9}
         // This is a presentational image, so the alt should be an empty string
         alt=''
-        src='../images/heroimage.png'
-        loading='eager'
-        placeholder='blurred'
-        objectPosition='top left'
-        objectFit='none'
-        quality='100'
+        src='../images/hero_photo_collage.png'
+        layout='fullWidth'
+        backgroundColor='rgba(0, 0, 0, 0.45)'
+        // aspectRatio={16 / 9}
+        // loading='eager'
+        // placeholder='blurred'
+        objectPosition='center center'
+        objectFit='contain'
+        // quality='100'
       />
       <HeroText
         style={{
