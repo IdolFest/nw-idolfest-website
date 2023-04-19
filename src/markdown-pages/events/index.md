bcb76207fde3b61bfdae37493d99f995b83c3267
SarahLynne <saralain88@gmail.com>
2023-04-18 23:42:54 -0400

    Bunch of apps

--- src/markdown-pages/events/index.md
+++ src/markdown-pages/events/index.md
@@ -4,7 +4,8 @@ title: Events
 template: page
 ---
 
-
 NW IdolFest is excited to provide a venue for your amazing panels and performances this year!
 
-Stay tuned for panel applications.
+Want to host a panel or performance? Submit [an application](http://idolfe.st/panelapp) by July 31st to have your event considered.
+
+Want to perform at our Northern Lights concert event? We have [an application for that too](https://idolfe.st/nlapp)!
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,23 @@ import Seo from '@components/seo'
 import PageContent from '@components/PageContent'
 import NewsletterSignup from '@components/NewsletterSignup'
 import PageHeader from '@components/PageHeader'
+import { Button, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+  buttonHolder: {
+    display: "flex",
+    "& > a": {
+      flexGrow: 1,
+      margin: "4px 32px",
+      width: "200px",
+      marginTop: "24px"
+    }
+  },
+}))
+
 
 const IndexPage = () => {
+  const classes = useStyles()
   return (
     <Layout>
       <Seo title="Home" />
@@ -21,6 +36,14 @@ const IndexPage = () => {
 
       <PageContent>
         <p>Get ready for NW IdolFest 2023!</p><br />
+        <p>We've recently opened applications for vendors, performers, and our Northern Lights concert! Click the links below to apply!</p>
+        <div className={classes.buttonHolder}>
+          <Button variant="contained" className="cta" href='https://idolfe.st/vendorapp'>Apply as a Vendor</Button>
+          <Button variant="contained" className="cta" href='http://idolfe.st/panelapp'>Apply as a Guest or Performer</Button>
+          <Button variant="contained" className="cta" href='https://idolfe.st/nlapp'>Apply for a Northern Lights performance</Button>
+        </div>
+        <br />
+        <br />
         <p>Sign up for our email list below to get notifications for future announcements.</p>
         <NewsletterSignup />
       </PageContent>
