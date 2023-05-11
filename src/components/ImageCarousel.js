d77fc40a3b367a121e902c2c60ccd2dd3059f95b
SarahLynne <saralain88@gmail.com>
2023-05-11 00:17:40 -0400

    Trying a new carousel

---
+++ src/components/ImageCarousel.js
@@ -0,0 +1,110 @@
+import React, {useEffect, useState, useCallback, createRef} from  'react'
+import { makeStyles } from '@material-ui/styles'
+import { GatsbyImage } from 'gatsby-plugin-image'
+
+const useStyles = makeStyles(theme => ({
+  carouselSlider: {
+    width: "100%",
+    maxWidth: "800px",
+    height: "400px",
+    position: "relative",
+    overflow: "hidden",
+  },
+  carouselSlide: {
+    width: "100%",
+    maxWidth: "800px",
+    height: "400px",
+    position: "absolute",
+    transition: "all 0.5s",
+    textAlign: "center",
+
+    '& > div': {
+      height: "100%",
+      objectFit: "cover"
+    }
+  },
+  btn: {
+    position: "absolute",
+    width: "40px",
+    height: "40px",
+    padding: "10px",
+    border: "none",
+    borderRadius: "50%",
+    zIndex: 10,
+    cursor: "pointer",
+    backgroundColor: theme.palette.light_pink,
+    fontSize: "18px",
+    lineHeight: "18px",
+
+    '&:active': {
+      transform: "scale(1.1)",
+      backgroundColor: theme.palette.dark_pink
+    }
+  },
+  btnPrev: {
+    top: "45%",
+    left: "2%"
+  },
+  btnNext: {
+    top: "45%",
+    right: "2%"
+  }
+}))
+
+
+const ImageCarousel = (props) => {
+  const classes = useStyles()
+  const sliderRef = createRef()
+  const [currentSlide, updateSlide] = useState(0)
+  const [hasInitialized, setInitialized] = useState(false)
+  
+  useEffect(() => {
+    if (!hasInitialized) { 
+      setInitialized(true)
+      const slides = document.querySelectorAll(`.${classes.carouselSlide}`)
+      slides.forEach((slide, idx) => {
+        slide.style.transform = `translateX(${idx * 100}%)`
+      })
+    } else {
+
+      const slides = sliderRef.current.querySelectorAll(`.${classes.carouselSlide}`)
+      slides.forEach((slide, idx) => {
+        slide.style.transform = `translateX(${100 * (idx - currentSlide)}%)`
+      })
+    }
+  }, [hasInitialized, setInitialized, classes, currentSlide, sliderRef])
+
+  const goNext = useCallback(() => {
+    console.info('next', currentSlide, props.images, updateSlide)
+    if (currentSlide < props.images.length) {
+      updateSlide(currentSlide + 1)
+    } else {
+      updateSlide(0);
+    }
+  }, [currentSlide, updateSlide, props])
+
+  const goPrev = useCallback(() => {
+    console.info('prev', currentSlide, props.images, updateSlide)
+
+    if (currentSlide > 0) {
+      updateSlide(currentSlide - 1)
+    } else {
+      updateSlide(props.images.length - 1);
+    }
+  }, [currentSlide, updateSlide, props])
+
+
+  const imageFiles = props.images;
+  return (
+      <div className={`${classes.carouselSlider}`} ref={sliderRef}>
+          {imageFiles.map(img => 
+            <div className={`${classes.carouselSlide}`} style={{animationDuration: props.animationDuration}} key={img.relativePath}>
+              <GatsbyImage image={img.childImageSharp.gatsbyImageData} placeholder="blurred" alt="Prior event" />
+            </div>)}
+        <button className={`${classes.btn} ${classes.btnNext}`} onClick={goNext}>&gt;</button>
+        <button className={`${classes.btn} ${classes.btnPrev}`} onClick={goPrev}>&lt;</button>
+      </div>
+  )
+}
+
+export default ImageCarousel
--- src/pages/testhome.js
+++ src/pages/testhome.js
@@ -5,8 +5,8 @@ import PageContent from '@components/PageContent'
 import NewsletterSignup from '@components/NewsletterSignup'
 import PageHeader from '@components/PageHeader'
 import { graphql, useStaticQuery } from 'gatsby'
-import { makeStyles } from '@material-ui/core'
-import ImageMarquee from '../components/ImageMarquee'
+import { makeStyles, Button } from '@material-ui/core'
+import ImageCarousel from "../components/ImageCarousel"
 
 const useStyles = makeStyles(theme => ({
   introWrapper: {
@@ -27,6 +27,24 @@ const useStyles = makeStyles(theme => ({
     width: "100%",
     backgroundColor: "#fff",
     height: "4px"
+  },
+  carouselHolder: {
+    margin: "auto",
+    display: "grid",
+    placeItems: "center",
+    marginBottom: "24px",
+  },
+  buttonHolder: {
+    display: "flex",
+    flexWrap: "wrap",
+    "& > a": {
+      flexGrow: 1,
+      margin: "4px 32px",
+      width: "200px",
+      flexBasis: "200px",
+      marginTop: "24px",
+      textAlign: "center"
+    }
   }
 }))
 
@@ -41,7 +59,7 @@ const IndexPage = () => {
         relativePath
         childImageSharp {
           gatsbyImageData(
-            height: 250
+            height: 400
           )
         }
       }
@@ -53,23 +71,23 @@ const IndexPage = () => {
     <Layout>
       <Seo title="Home" />
 
-      <div className={classes.introWrapper}>
-
-          <ImageMarquee images={imageFiles} animationDuration="300s" direction="left" />
-          <div className={classes.introLine} />
-          <ImageMarquee images={imageFiles} animationDuration="300s" direction="right" />
-          <h1 className={classes.introText}>
-            Announcing NW IdolFest 2023!
-          </h1>
-      </div>
-
       <PageHeader 
         title="Announcing NW IdolFest 2023!" 
       />
 
       <PageContent>
-        <br />
+        <div className={classes.carouselHolder}>
+          <ImageCarousel images={imageFiles} />
+        </div>
         <p>Get ready for NW IdolFest 2023!</p><br />
+        <p>We've recently opened applications for vendors, performers, and our Northern Lights concert! Click the links below to apply!</p>
+        <div className={classes.buttonHolder}>
+          <Button variant="contained" className="cta" href='https://idolfe.st/vendorapp'>Apply as a Vendor</Button>
+          <Button variant="contained" className="cta" href='http://idolfe.st/panelapp'>Apply as a Panelist or Performer</Button>
+          <Button variant="contained" className="cta" href='https://idolfe.st/nlapp'>Apply for a Northern Lights performance</Button>
+        </div>
+        <br />
+        <br />
         <p>Sign up for our email list below to get notifications for future announcements.</p>
         <NewsletterSignup />
       </PageContent>
