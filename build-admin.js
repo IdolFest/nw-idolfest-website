7f788303d41998e410ff9e89321b7c02e0e905d2
SarahLynne <saralain88@gmail.com>
2023-05-14 00:04:15 -0400

    Make previews look like the site

--- build-admin.js
+++ build-admin.js
@@ -2,11 +2,11 @@
 const fs = require('fs')
 const path = require('path')
 
-const cssFile = fs.readdirSync('./public').filter(f => f.startsWith('styles-') && f.endsWith('.css'))[0]
+const cssFile = fs.readdirSync('./public').filter(f => f.startsWith('styles.') && f.endsWith('.css'))[0]
 const cacheBuster = Date.now()
 
 // Drop the name of the gatsby css file into our javascript helper
-const adminJs = fs.readFileSync('./static/admin/admin.js').toString().replace('{{GATSBY_CSS}', `/${cssFile}`)
+const adminJs = fs.readFileSync('./static/admin/admin.js').toString().replace('{{GATSBY_CSS}}', `/${cssFile}`)
 // Make sure the css we're including for both the page and the iframe actually reload. FF is whiny.
 const adminHtml = fs.readFileSync('./static/admin/index.html').toString().replace(/{{CACHEBUST}}/g, cacheBuster)
 
--- static/admin/admin.js
+++ static/admin/admin.js
@@ -22,8 +22,70 @@ CMS.registerEventListener({
         return entry.get('data').set('slug', titleSlug);
         }
     },
-    });
+});
 
-    // Replaced with generated css during build process
-    CMS.registerPreviewStyle("{{GATSBY_CSS}}");
-    CMS.registerPreviewStyle("/admin/editor-preview.css");
+// Replaced with generated css during build process
+CMS.registerPreviewStyle("/admin/editor-preview.css")
+CMS.registerPreviewStyle("{{GATSBY_CSS}}");
+
+
+var PostPreview = createClass({
+    render: function() {
+        const entry = this.props.entry
+
+        // We don't have jsx here sadly, so do things the ugly way
+        return h('div', {},
+            h('div', {"className": "heading-wrapper"},
+                h('h1', {}, entry.getIn(['data', 'title']))),
+            h('div', {"className": "text post-container"}, 
+                h('p', {"className": "post-date"},
+                    "Posted: ",
+                    entry.getIn(['data', 'date']) ?? "Today"),
+                h('h2', {}, entry.getIn(['data', 'title'])),
+
+                this.props.widgetFor('body'))
+        )
+    }
+})
+  
+CMS.registerPreviewTemplate("blog-posts", PostPreview)
+
+var PagePreview = createClass({
+    render: function() {
+        const entry = this.props.entry
+
+        // We don't have jsx here sadly, so do things the ugly way
+        return h('div', {},
+            h('div', {"className": "heading-wrapper"},
+                h('h1', {}, entry.getIn(['data', 'title']))),
+            h('div', {"className": "text post-container"}, 
+                this.props.widgetFor('body'))
+        )
+    }
+})
+
+CMS.registerPreviewTemplate("root-pages", PagePreview)
+CMS.registerPreviewTemplate("about", PagePreview)
+CMS.registerPreviewTemplate("events", PagePreview)
+
+var NavPreview = createClass({
+    render: function() {
+        const entry = this.props.entry;
+        console.info('entry', entry, this.props.fieldsMetaData.getIn(['data', 'headersData']))
+        const navItems = entry.getIn(['data', 'headersData'])        
+  
+        return h('div', {className: "nav-holder"},
+            // h('h1', {}, title),
+            h('ul', {},
+                ...navItems.map(navItem => 
+                    h('li', {"className": navItem.getIn(['disabled'] === true ? "hidden": "visible")}, navItem.getIn(['label']), 
+                        (navItem.getIn(['children']) ?? []).map(navChild => 
+                            h('li', {"style": {marginLeft: "1em"}, "className": navChild.getIn(['disabled'] === true ? "hidden": "visible")}, navChild.getIn(['label']))))
+                )
+  
+            )
+        ) 
+    }
+})
+
+CMS.registerPreviewTemplate("navigation", NavPreview)
