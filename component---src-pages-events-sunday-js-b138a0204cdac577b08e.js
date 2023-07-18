c3dede567d9c73b6832ad6a7ee169e5fe1578269
saralain <saralain@users.noreply.github.com>
2023-07-18 00:53:17 +0000

    deploy: e76e76319284077982083d3c1accc867e97968d6

---
+++ component---src-pages-events-sunday-js-b138a0204cdac577b08e.js
@@ -0,0 +1,2 @@
+(self.webpackChunknw_idolfest_website=self.webpackChunknw_idolfest_website||[]).push([[866],{3064:function(e,t,l){"use strict";l.r(t);var n=l(7294),a=l(2207),u=l(5583),c=l(6273),r=l(5954),i=l(6900),d=l(6892);t.default=function(){return n.createElement(a.Z,null,n.createElement(u.Z,{title:"Sunday Schedule"}),n.createElement(r.Z,{title:"Sunday Schedule"}),n.createElement(c.Z,{maxWidth:"xl"},n.createElement(i.Z,null,n.createElement(d.Z,{day:"2022-10-23"}))))}}}]);
+//# sourceMappingURL=component---src-pages-events-sunday-js-b138a0204cdac577b08e.js.map
