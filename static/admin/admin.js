b4ef5da1a57b01fb657a289ce304efd7a9c2e9c6
SarahLynne <saralain88@gmail.com>
2023-08-01 13:18:26 -0400

    slug (   o_O)

--- static/admin/admin.js
+++ static/admin/admin.js
@@ -9,7 +9,7 @@ CMS.registerEventListener({
         const p = new RegExp(a.split('').join('|'), 'g')
 
         return string.toString().toLowerCase()
-            .replace(/\s+/g, '-') // Replace spaces with -
+            .replace(/\s+/g, '') // Replace spaces with nothing
             .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
             .replace(/&/g, '-and-') // Replace & with 'and'
             .replace(/[^\w\-]+/g, '') // Remove all non-word characters
